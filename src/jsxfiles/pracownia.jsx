715a8ca4608df8988ee92ca6907de6d3a3855ab1
NorbertZdziarski <norbertzdz@gmail.com>
2023-06-22 18:36:24 +0200

    poprawiono wyświetlanie zdjęcia w zakładce pracownia

--- src/jsxfiles/pracownia.jsx
+++ src/jsxfiles/pracownia.jsx
@@ -24,15 +24,17 @@ function Pracownia() {
                 </header>
                 <div className="pracownia_box">
                     <div className="pracownia_column mainfont pracownia_section">
+
+                        {windowWidth < 950 && (
+                            <img src={michalDabekPhotoMobile} alt="Michał Dąbek" className=" pracownia_img_mobile mobile"/>
+                        )}
                         <p >PRACOWNIA PROJEKTOWA M KWADRAT, to autorska pracownia architektoniczna, działająca formalnie od 2004 roku. Zajmuje się projektowaniem architektonicznym w szerokiej skali – zaczynając od projektów wnętrz, poprzez projekty budynków mieszkalnych i użyteczności publicznej, a kończąc na przestrzeniach publicznych placów miejskich oraz budowlach inżynierskich.</p>
+
                         <p >Sprawdzona przez lata w praktyce metoda tworzenia indywidualnych zespołów zadaniowych z wyspecjalizowanymi projektantami, pozwala z dużą elastycznością, kreatywnością i zaangażowaniem finalizować złożony i trudny proces projektowy. Równie ważnym obszarem działań pracowni projektowej jest czynny udział w międzynarodowych i krajowych konkursach projektowych, co pozwala stale konfrontować swoje pomysły ze współczesnymi wizjami architektonicznymi w wydaniu niekomercyjnym. Z tego też powodu jesteśmy szczególnie dumni z licznych nagród i wyróżnień uzyskanych w ramach profesjonalnego współzawodnictwa.</p>
                         <p >Zapraszamy do współpracy inwestorów i podmioty z branży, dla których świadome projektowanie jest jednym z kluczowych czynników determinujących jakość i sukces inwestycji.</p>
                     </div>
                     {windowWidth >= 950 && (
                         <img src={michalDabekPhoto} alt="Michał Dąbek" className="pracownia_img screen"/>
-                    )}
-                    {windowWidth < 950 && (
-                        <img src={michalDabekPhotoMobile} alt="Michał Dąbek" className=" pracownia_img_mobile mobile"/>
                     )}
 
                 </div>
